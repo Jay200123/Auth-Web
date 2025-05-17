01b1dc4ee8e33fec108893f0f4101809ae7fb6fc
Jay200123 <kenken.ayonon@gmail.com>
2025-05-17 20:08:15 +0800

    main: ADDED authentication logic to Navbar component

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,7 +1,9 @@
 import { useNavigate } from "react-router-dom";
+import { useStore } from "../../state/store";
 
 export default function Navbar() {
   const navigate = useNavigate();
+  const { auth, isAuthenticated, logout } = useStore();
 
   const handleHome = () => {
     navigate("/");
@@ -14,11 +16,21 @@ export default function Navbar() {
     navigate("/register");
   };
 
+  const handleLogout = async () => {
+    if (window.confirm("Are you sure you want to logout?")) {
+      logout();
+      navigate("/");
+    }
+  };
+
   return (
     <>
       <nav className="w-full lg:h-16 md:h-14 h-12 flex items-center justify-between overflow-hidden border border-gray-400 p-2 shadow-md">
         <div className="p-2">
-          <h3 onClick={handleHome} className="lg:text- lg:font-bold font-medium md:text-base text-sm">
+          <h3
+            onClick={handleHome}
+            className="lg:text- lg:font-bold font-medium md:text-base text-sm"
+          >
             Auth Web
           </h3>
         </div>
@@ -26,14 +38,28 @@ export default function Navbar() {
           <input type="text" className="border border-black rounded-md p-1" />
         </div>
         <div className="">
-          <ul className="flex items-center gap-4">
-            <li onClick={handleLogin} className="font-medium cursor-pointer">
-              Login
-            </li>
-            <li onClick={handleRegister} className="font-medium cursor-pointer">
-              Register
-            </li>
-          </ul>
+          {auth && isAuthenticated ? (
+            <ul className="flex items-center gap-4">
+              <li onClick={handleHome} className="font-medium cursor-pointer">
+                Home
+              </li>
+              <li onClick={handleLogout} className="font-medium cursor-pointer">
+                Logout
+              </li>
+            </ul>
+          ) : (
+            <ul className="flex items-center gap-4">
+              <li onClick={handleLogin} className="font-medium cursor-pointer">
+                Login
+              </li>
+              <li
+                onClick={handleRegister}
+                className="font-medium cursor-pointer"
+              >
+                Register
+              </li>
+            </ul>
+          )}
         </div>
       </nav>
     </>
