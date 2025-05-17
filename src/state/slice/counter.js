a95615afb859319879931f54dcf47dbf1761db85
Jay200123 <kenken.ayonon@gmail.com>
2025-05-17 16:38:42 +0800

    main: ADDED counter slice

---
+++ src/state/slice/counter.js
@@ -0,0 +1,18 @@
+import { create } from "zustand";
+
+export const useCounterStore = create((set) => ({
+  count: 0,
+  increment: () => {
+    set((state) => ({
+      count: state.count + 1,
+    }));
+  },
+}));
+/**
+ * The `useCounterStore` hook can be used in your components to access the state and actions.
+ * For example:
+ *
+ * const { count, increment } = useCounterStore();
+ *
+ * This will give you access to the `count` state and the `increment` action.
+ */
