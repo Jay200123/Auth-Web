12d7b12b8b361ff92e4bf32b31480ce0aadd5087
Jay200123 <kenken.ayonon@gmail.com>
2025-05-17 20:09:14 +0800

    main: REFACTORED useCounterStore to remove unnecessary create import

--- src/state/slice/counter.js
+++ src/state/slice/counter.js
@@ -1,13 +1,11 @@
-import { create } from "zustand";
-
-export const useCounterStore = create((set) => ({
+export const useCounterStore = (set) => ({
   count: 0,
   increment: () => {
     set((state) => ({
       count: state.count + 1,
     }));
   },
-}));
+});
 /**
  * The `useCounterStore` hook can be used in your components to access the state and actions.
  * For example:
