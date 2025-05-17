942f1c82f5ba2f39134bb8b636f4cacbd238a427
Jay200123 <kenken.ayonon@gmail.com>
2025-05-17 20:14:02 +0800

    main: ADDED ProtectedRoutes component to handle authentication and navigation

--- src/components/ProtectedRoutes/index.jsx
+++ src/components/ProtectedRoutes/index.jsx
@@ -1,3 +1,11 @@
+import { useStore } from "../../state/store";
+import { Navigate } from "react-router-dom";
+
 export default function ProtectedRoutes({ children }) {
+  const { auth, isAuthenticated } = useStore();
+
+  if (!auth && !isAuthenticated) {
+    return <Navigate to="/login" replace />;
+  }
   return <>{children}</>;
 }
