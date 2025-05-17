a2f0ce6f31729f6c863de19929b1c4e84dc69252
Jay200123 <kenken.ayonon@gmail.com>
2025-05-17 20:08:42 +0800

    main: ADDED formik integration for login form handling

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,7 +1,29 @@
+import { useFormik } from "formik";
+import { useNavigate } from "react-router-dom";
+import { useStore } from "../../state/store.js";
+
 export default function Login() {
+  const navigate = useNavigate();
+  const { login } = useStore();
+
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema: null,
+    onSubmit: async (values) => {
+      const data = await login(values.email, values.password);
+
+      navigate("/");
+    },
+  });
   return (
     <>
-      <form className="flex items-center justify-center">
+      <form
+        onSubmit={formik.handleSubmit}
+        className="flex items-center justify-center"
+      >
         {/* Login  page forms */}
         <div className="flex flex-col justify-between lg:w-96 lg:h-96 md:w-80 md:h-80 h-64 w-64 border border-black rounded-md p-2.5">
           <h3 className="lg:text-2xl md:text-lg text-base text-center font-medium">
@@ -11,20 +33,33 @@ export default function Login() {
             <label className="font-medium text-sm lg:text-base">Email</label>
             <input
               type="text"
+              id="email"
+              name="email"
+              onBlur={formik.handleBlur}
+              onChange={formik.handleChange}
+              value={formik.values.email}
               placeholder="text"
-              className="w-full lg:p-1.5 md:p-1 p-0.5 rounded-sm border border-gray-400"
+              className="w-full lg:text-base md:text-sm text-xs lg:p-1.5 md:p-1 p-0.5 rounded-sm border border-gray-400"
             />
           </div>
           <div className="flex flex-col gap-2 mt-4">
             <label className="font-medium text-sm lg:text-base">Password</label>
             <input
-              type="password" 
+              type="password"
+              id="password"
+              name="password"
+              onBlur={formik.handleBlur}
+              onChange={formik.handleChange}
+              value={formik.values.password}
               placeholder="text"
-              className="w-full lg:p-1.5 md:p-1 p-0.5 rounded-sm  border border-gray-400"
+              className="w-full lg:text-base md:text-sm text-xs lg:p-1.5 md:p-1 p-0.5 rounded-sm  border border-gray-400"
             />
           </div>
           <div className="gap-2 mt-4">
-            <button className="w-full bg-blue-500 rounded-md lg:text-lg font-medium md:text-base text-sm text-white p-2">
+            <button
+              type="submit"
+              className="w-full bg-blue-500 rounded-md lg:text-lg font-medium md:text-base text-sm text-white p-2"
+            >
               Sign In
             </button>
           </div>
