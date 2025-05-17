5441fdb7dc2348cfa65eb4872be179523fb0b9e4
Jay200123 <kenken.ayonon@gmail.com>
2025-05-17 20:09:21 +0800

    main: REFACTORED useGreetingsStore to remove unnecessary create import

--- src/state/slice/greetings.js
+++ src/state/slice/greetings.js
@@ -1,14 +1,12 @@
-import { create } from "zustand";
-
 /**
  * Create a basic zustand store hook for managing a greeting message.
  * The store contains a single state variable `message` and a method (action) `setMessage`
  * to update the message to "Hello, World!".
  */
-export const useGreetingsStore = create((set) => ({
+export const useGreetingsStore = (set) => ({
   message: "",
 
   setMessage: () => {
     set({ message: "Hello, World!" });
   },
-}));
+});
