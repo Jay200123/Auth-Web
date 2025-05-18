d8afa21f4fb4fb3b5012a5c9b02ead4692d6998b
Jay200123 <kenken.ayonon@gmail.com>
2025-05-18 16:59:13 +0800

    main: ADDED ProtectedRoutes to index.js for improved component exports

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import ProtectedRoutes from "./ProtectedRoutes";
 
-export { Navbar, Footer };
+export { Navbar, Footer, ProtectedRoutes };
