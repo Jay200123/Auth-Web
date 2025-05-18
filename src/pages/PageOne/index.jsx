771ea943ded82c63c5ebba32ee0e203821995cd5
Jay200123 <kenken.ayonon@gmail.com>
2025-05-18 16:59:39 +0800

    main: ADDED PageOne component for protected route display

---
+++ src/pages/PageOne/index.jsx
@@ -0,0 +1,12 @@
+export default function PageOne() {
+  return (
+    <>
+      <div className="flex justify-center w-full h-screen items-center">
+        <h3 className="lg:text-lg md:text-base text-sm">This is Page One</h3>
+        <p className="">
+          Its a <span className="">protected Route</span>
+        </p>
+      </div>
+    </>
+  );
+}
