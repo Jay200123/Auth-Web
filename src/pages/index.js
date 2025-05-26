d134c3cc4ce4d0273a35ca30737f69865b0d80e6
Jay200123 <kenken.ayonon@gmail.com>
2025-05-26 17:37:47 +0800

    main: ADDED Register component to the export list in index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,11 @@
 import Home from "./Home";
 import Login from "./Login";
+import Register from "./Register";
 import PageOne from "./PageOne";
 
-export { Home, Login, PageOne };
+export { 
+    Home, 
+    Login, 
+    Register, 
+    PageOne
+};
