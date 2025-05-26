2ebaedcd7ee958896127db6b9ba0dd26ac2ac206
Jay200123 <kenken.ayonon@gmail.com>
2025-05-26 17:36:58 +0800

    main: ADDED login validation schema for user authentication

---
+++ src/validations/loginValidation.js
@@ -0,0 +1,14 @@
+import * as yup from "yup";
+
+const loginValidationSchema = yup.object({
+  email: yup
+    .string()
+    .email("Invalid email format")
+    .required("Email is required"),
+  password: yup
+    .string()
+    .min(6, "Password must be at least 6 characters long")
+    .required("Password is required"),
+});
+
+export default loginValidationSchema;
