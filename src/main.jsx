8391d821901bb5f19dc5b87d4e2daea3f75ab5fc
Jay200123 <kenken.ayonon@gmail.com>
2025-05-26 17:37:55 +0800

    main: ADDED ToastContainer for improved user notifications

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ToastContainer } from "react-toastify";
 import "./index.css";
 import App from "./App.jsx";
 
@@ -10,6 +11,7 @@ const queryClient = new QueryClient();
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
+      <ToastContainer />
       <App />
     </QueryClientProvider>
   </StrictMode>
